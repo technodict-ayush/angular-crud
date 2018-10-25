0caa6f4c4ede1e476adda59090142cf06b675e09
ayush <121ayushjain@gmail.com>
2018-10-25 16:47:11 +0530

    i am commiting

---
+++ src/app/employees/employee/employee.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {EmployeeService} from '../shared/employee.service';
+
+import {NgForm} from '@angular/forms';
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private employeeService : EmployeeService) { }
+
+  ngOnInit() {
+    
+  }
+
+  resetForm(form : NgForm){
+    
+    form.reset();
+    this.employeeService.selectedEmployee = {
+      EmployeeID: null,
+      FirstName : '',
+      LastName : '',
+      EmpCode : '',
+      Position :'',
+      Office : ''
+
+    }
+
+
+  }
+
+}
---
+++ src/app/employees/shared/employee.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import {Employee} from './employee.model'
+@Injectable()
+export class EmployeeService {
+  selectedEmployee : Employee;
+  constructor() { }
+}
